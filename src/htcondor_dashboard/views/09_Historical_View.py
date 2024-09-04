535ad162b556ba8d399167aba42febfa0a982561
kreczko <lkreczko@googlemail.com>
2024-09-04 15:36:27 +0100

    Mark historical view as under construction

--- src/htcondor_dashboard/views/09_Historical_View.py
+++ src/htcondor_dashboard/views/09_Historical_View.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.markdown("# Historical View (WIP)")
+st.markdown("# Historical View 🚧")
+
+st.markdown("## Last 24h")
+
+st.markdown("## Last 7 days")
+
+st.markdown("## Last 30 days")
+
+st.markdown("## Last 365 days")
