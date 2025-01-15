8a4b70772ae7ebe7535c56c559826fc8a9254807
kreczko <lkreczko@googlemail.com>
2025-01-15 14:53:59 +0000

    add config options

---
+++ src/htcondor_dashboard/config.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
+from fastapi.templating import Jinja2Templates
+
+class Settings(BaseSettings):
+    exclude_submit_nodes: List[str] = []
+    model_config = SettingsConfigDict(env_prefix='HTDASH_')
+
+
+@lru_cache
+def get_settings():
+    # os.environ['HTDASH_exclude_submit_nodes'] = '["status.dice.priv","submit-3"]'
+    # os.environ['HTDASH_EXCLUDE_SUBMIT_NODES'] = '["status.dice.priv","submit-2"]'
+    try:
+        settings = Settings()
+    except Exception as _:
+        settings = Settings(exclude_submit_nodes=[])
+    return settings
+
+def get_template_dir():
+    return Jinja2Templates(directory="templates")
