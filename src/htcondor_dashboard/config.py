da93a8c714a4c259b77450bbf965f7e387c024de
kreczko <lkreczko@googlemail.com>
2025-01-23 16:41:16 +0000

    fix: tuple definition for config.exclude_submit_nodes

--- src/htcondor_dashboard/config.py
+++ src/htcondor_dashboard/config.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import Tuple, Union
 
 from fastapi.templating import Jinja2Templates
 from pydantic import ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-ListSetting = Union[Tuple[str], Tuple[()]]
 
 
 class Settings(BaseSettings):
@@ -16,7 +13,7 @@ class Settings(BaseSettings):
     Settings can be set via environment variables starting with HTDASH_.
     """
 
-    exclude_submit_nodes: ListSetting
+    exclude_submit_nodes: tuple[str, ...]
     grafana_url: str = "http://localhost:3000"
     model_config = SettingsConfigDict(env_prefix="HTDASH_")
 
@@ -26,7 +23,6 @@ def get_settings() -> Settings:
     """Cached function to get the settings for the HTCondor Dashboard.
     Sets defaults if the settings are not found.
     """
-    # os.environ['HTDASH_exclude_submit_nodes'] = '["status.dice.priv","submit-3"]'
     try:
         settings = Settings()  # type: ignore[call-arg]
     except ValidationError as _:
